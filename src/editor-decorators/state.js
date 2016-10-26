6c2bdd644710abca17f967f1af7a513626684c38
Benjamin Kniffler <bkniffler@me.com>
2016-10-26 18:26:50 +0100

    Allow to define terse in state plugin

--- src/editor-decorators/state.js
+++ src/editor-decorators/state.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import { Raw, Plain } from 'slate';
 import { batch } from '../utils/batch';
 
-const parseValue = (value, initialState) => {
-  try { return Raw.deserialize(value, { terse: true }); }
-  catch (err) { return initialState ? Raw.deserialize(initialState, { terse: true }) : Plain.deserialize(''); }
+const parseValue = (value, initialState, terse) => {
+  try { return Raw.deserialize(value, { terse }); }
+  catch (err) {
+    try { return Raw.deserialize(value, { terse: !terse }); }
+    catch(err){
+      return initialState ? Raw.deserialize(initialState, { terse }) : Plain.deserialize('');
+    }
+  }
 };
 const defaultGetValue = ({ value }) => value;
 const defaultChangeValue = ({ onChange }, value) => onChange ? onChange(value) : null;
 
-export default (getValue = defaultGetValue, changeValue = defaultChangeValue, opt = {}) => {
+export default ({ getValue = defaultGetValue, changeValue = defaultChangeValue, initialState, terse } = {}) => {
   return Editor => class SlateStateDecorator extends Component {
     static propTypes = { }
     isFocused = false;
@@ -19,14 +24,14 @@ export default (getValue = defaultGetValue, changeValue = defaultChangeValue, op
     constructor(props) {
       super();
       this.rawValue = getValue(props);
-      this.value = parseValue(this.rawValue, opt.initialState);
+      this.value = parseValue(this.rawValue, initialState, terse);
     }
 
     shouldComponentUpdate(props) {
       const newValue = getValue(props);
       const oldValue = getValue(this.props);
       if (newValue !== oldValue && this.rawValue !== newValue) {
-        this.value = parseValue(newValue);
+        this.value = parseValue(newValue, undefined, terse);
         return true;
       } return false;
     }
@@ -35,7 +40,7 @@ export default (getValue = defaultGetValue, changeValue = defaultChangeValue, op
       this.value = value;
       this.forceUpdate();
       if (changeValue) {
-        const rawValue = Raw.serialize(value, { terse: true });
+        const rawValue = Raw.serialize(value, { terse });
         if (JSON.stringify(this.rawValue) !== JSON.stringify(rawValue)) {
           this.rawValue = rawValue;
           this.batch(() => changeValue(this.props, rawValue));
